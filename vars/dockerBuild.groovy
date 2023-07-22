95e39325bda4172d107b3993a7d3b01d2defd62a
ugvenkat <ugvenkat@hotmail.com>
2023-07-22 15:00:53 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,13 @@
-// def call(String project, String ImageTag, String hubUser){
+def call(String hubUser, String AppName, String ImageTag, ){
     
-//     sh """
-//      docker image build -t ${hubUser}/${project} . 
-//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-//     """
-// }
+     sh """
+      docker image build -t ${hubUser}/${project} . 
+      docker image tag ${hubUser}/${AppName} ${hubUser}/${AppName}:${ImageTag}
+      docker image tag ${hubUser}/${AppName} ${hubUser}/${AppName}:latest
+     """
+}
 
+/*
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
@@ -14,3 +15,4 @@ def call(String aws_account_id, String region, String ecr_repoName){
      docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
+*/
