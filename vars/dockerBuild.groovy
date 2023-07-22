e04e058737fc50f081aa9d97a6af23007c19f141
ugvenkat <ugvenkat@hotmail.com>
2023-07-22 15:33:02 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(String hubUser, String AppName, String ImageTag ){
     
      sh """
-      docker image build -t ${hubUser}/${project} . 
+      docker image build -t ${hubUser}/${AppName} . 
       docker image tag ${hubUser}/${AppName} ${hubUser}/${AppName}:${ImageTag}
       #docker image tag ${hubUser}/${AppName} ${hubUser}/${AppName}:latest
      """
