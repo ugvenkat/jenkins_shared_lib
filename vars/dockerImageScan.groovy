cf6bc66767dbbca2cc4e7e85ba8e44cd6f2f627c
ugvenkat <ugvenkat@hotmail.com>
2023-07-22 15:03:12 -0500

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,11 +1,13 @@
-// def call(String project, String ImageTag, String hubUser){
+def call(String hubUser, String AppName, String ImageTag,){
     
-//     sh """   
-//      trivy image ${hubUser}/${project}:latest > scan.txt
-//      cat scan.txt
-//     """
-// }
+     sh """   
+      trivy image ${hubUser}/${AppName}:latest > scan.txt
+      cat scan.txt
+     """
+}
 
+
+/*
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
@@ -13,3 +15,4 @@ def call(String aws_account_id, String region, String ecr_repoName){
     cat scan.txt
     """
 }
+*/
