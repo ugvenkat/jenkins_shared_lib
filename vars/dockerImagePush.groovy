3b56d61acd3587ea9cb1169296b4b9165a03b39f
ugvenkat <ugvenkat@hotmail.com>
2023-07-22 15:08:24 -0500

    Update dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,16 +1,18 @@
-// def call(String project, String ImageTag, String hubUser){
-//     withCredentials([usernamePassword(
-//             credentialsId: "docker",
-//             usernameVariable: "USER",
-//             passwordVariable: "PASS"
-//     )]) {
-//         sh "docker login -u '$USER' -p '$PASS'"
-//     }
-//     sh "docker image push ${hubUser}/${project}:${ImageTag}"
-//     sh "docker image push ${hubUser}/${project}:latest"   
-// }
+def call(String project, String ImageTag, String hubUser){
+     withCredentials([usernamePassword(
+              credentialsId: "docker",
+              usernameVariable: "USER",
+              passwordVariable: "PASS"
+      )]) {
+         sh "docker login -u '$USER' -p '$PASS'"
+       }
+       sh "docker image push ${hubUser}/${project}:${ImageTag}"
+       sh "docker image push ${hubUser}/${project}:latest"   
+}
 
 
+
+/*
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
@@ -18,3 +20,4 @@ def call(String aws_account_id, String region, String ecr_repoName){
      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
+*/
